5824ea7291232b6473df16d08af2e168b180f6cf
Eemil Lähteenmäki <eemil.lahteenmaki@gmail.com>
2024-05-04 21:11:37 +0300

    Add logging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import { logger } from "@/utils/logger";
 import Link from "next/link";
 
 export default function HomePage() {
+  logger.info("Rendering Home page");
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
