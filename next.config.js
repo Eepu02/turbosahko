d361fd1d336c8d05dd77530a45b68286b606e627
Eemil Lähteenmäki <eemil.lahteenmaki@gmail.com>
2024-07-27 19:51:20 +0300

    Increase SSG timeout

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  staticPageGenerationTimeout: 60 * 3, // 3 minutes
+};
 
 export default config;
